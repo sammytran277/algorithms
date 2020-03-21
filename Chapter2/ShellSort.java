78bfc2a131f93b9c731307948f30905d6c784c31
st974 <st974@drexel.edu>
2020-03-21 10:29:02 -0400

    Added ShellSort.java

---
+++ Chapter2/ShellSort.java
@@ -0,0 +1,60 @@
+/* Adapted code from "Algorithms" by Robert Sedgewick and Thomas Wayne 
+   to implement the elementary shell sort algorithm */
+
+   import java.util.Scanner;
+
+   public class ShellSort
+   {
+        public static void sort(int[] array)
+        {
+            int h = 1;
+
+            /* Donald Knuth's 3x + 1 incrementing sequence:
+               1, 4, 13, 40, 121, 364, ... */
+            while (h < array.length / 3)
+                h = (3 * h) + 1;
+            
+            while (h >= 1)
+            {
+                // h-sort the array
+                for (int i = h; i < array.length; i++)
+                {
+                    for (int j = i; j >= h && array[j] < array[j - h]; j -= h)
+                        swap(array, j, j - h);
+                }
+
+                h /= 3;
+            }
+       }
+   
+        private static void swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+   
+        public static void main(String[] args)
+        {    
+            Scanner s = new Scanner(System.in);
+   
+            System.out.print("Enter the array length: ");
+            int arrayLength = s.nextInt();
+   
+            int[] unsortedArray = new int[arrayLength];
+    
+            System.out.print("Enter the array values: ");
+            for (int i = 0; i < arrayLength; i++)
+                unsortedArray[i] = s.nextInt();
+    
+            sort(unsortedArray);
+    
+            System.out.print("Sorted array: ");
+            for (int n : unsortedArray)
+            {
+                System.out.print(n + " ");
+            }
+   
+            s.close();
+        }
+   }
